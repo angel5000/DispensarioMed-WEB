a3e4df517835308805e5d2582be52555a296fa48
angel5000 <delllancer@gmail.com>
2024-05-21 22:07:33 -0500

    new file:   src/app/components/agendamiento/agendamiento.component.css 	new file:   src/app/components/agendamiento/agendamiento.component.html 	new file:   src/app/components/agendamiento/agendamiento.component.spec.ts 	new file:   src/app/components/agendamiento/agendamiento.component.ts 	new file:   src/app/components/cabecera/cabecera.component.css 	new file:   src/app/components/cabecera/cabecera.component.html 	new file:   src/app/components/cabecera/cabecera.component.spec.ts 	new file:   src/app/components/cabecera/cabecera.component.ts 	new file:   src/app/components/gestion-medico/gestion-medico.component.css 	new file:   src/app/components/gestion-medico/gestion-medico.component.html 	new file:   src/app/components/gestion-medico/gestion-medico.component.spec.ts 	new file:   src/app/components/gestion-medico/gestion-medico.component.ts 	new file:   src/app/components/login/login.component.css 	new file:   src/app/components/login/login.component.html 	new file:   src/app/components/login/login.component.spec.ts 	new file:   src/app/components/login/login.component.ts 	new file:   src/app/components/pie/pie.component.css 	new file:   src/app/components/pie/pie.component.html 	new file:   src/app/components/pie/pie.component.spec.ts 	new file:   src/app/components/pie/pie.component.ts 	new file:   src/app/components/venta/venta.component.css 	new file:   src/app/components/venta/venta.component.html 	new file:   src/app/components/venta/venta.component.spec.ts 	new file:   src/app/components/venta/venta.component.ts

---
+++ src/app/components/agendamiento/agendamiento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamiento',
+  standalone: true,
+  imports: [],
+  templateUrl: './agendamiento.component.html',
+  styleUrl: './agendamiento.component.css'
+})
+export class AgendamientoComponent {
+
+}
