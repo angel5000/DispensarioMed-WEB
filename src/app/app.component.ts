f596ebcd3dc8a9abcb1ae215fdd115df2607656f
angel5000 <delllancer@gmail.com>
2024-05-25 22:30:50 -0500

    modified:   src/app/app.component.ts 	modified:   src/app/components/login/login.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { CabeceraComponent } from './components/cabecera/cabecera.component';
 import { PieComponent } from './components/pie/pie.component';
 import { Router, NavigationEnd } from '@angular/router';
@@ -9,7 +9,7 @@ import { Router, NavigationEnd } from '@angular/router';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PieComponent, CabeceraComponent,RouterLink]
+    imports: [RouterOutlet, PieComponent, CabeceraComponent,RouterLink,CommonModule]
 })
 export class AppComponent {
   title = 'ProyectoDAWA-Grupo11-DispensarioMedico';
